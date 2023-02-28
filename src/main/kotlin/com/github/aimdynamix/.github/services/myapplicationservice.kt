420e28cf18e8bc84d77a669000be521cd1816598
lucienleighton <119469038+lucienleighton@users.noreply.github.com>
2023-02-27 22:55:13 -0500

    Create myapplicationservice.kt
    Signed-off-by: lucienleighton <119469038+lucienleighton@users.noreply.github.com>

---
+++ src/main/kotlin/com/github/aimdynamix/.github/services/myapplicationservice.kt
@@ -0,0 +1,10 @@
+package com.github.aimdynamix.github.services
+
+import com.github.aimdynamix.github.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
